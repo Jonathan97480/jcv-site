9cdc7c8044aee1a6d7c73c30cddce4e0124a5a32
Maakino <teddyequerre0@gmail.com>
2022-11-21 10:01:55 +0100

    add about section responsive

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, lazy } from "react";
 import { Link } from "react-router-dom";
 import { ApiGetAllCategories } from "../api/apiRequest";
 import Api from "../api/conf";
 import { apiCategories } from "../interface";
-import CSS from 'csstype';
+import CSS from "csstype";
 
 export default function Home() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -19,37 +19,52 @@ export default function Home() {
 
   return (
     <div className="home">
-
       <section className="hero">
+        <div className="hero__content max-w padding">
+          <h1 className="hero__title">JCV CONSULTING</h1>
 
-        <h1 className="hero__title">JCV CONSULTING</h1>
-
-        <p className="hero__text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit dignissim
-          sit tincidunt non. Laoreet at nibh elit, ridiculus ultrices
-          pellentesque tincidunt ut nunc.
-        </p>
-
+          <p className="hero__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit
+            dignissim sit tincidunt non. Laoreet at nibh elit, ridiculus
+            ultrices pellentesque tincidunt ut nunc.
+          </p>
+        </div>
       </section>
 
       <section className="about">
 
-        <div className="about__content">
-          <h2>JCV Consulting</h2>
-          <span>Lorem ipsum dolor sit amet consectetur.</span>
-          <p>
-            Lorem ipsum dolor sit amet consectetur. Faucibus vitae ornare eu
-            mattis pellentesque morbi et duis condimentum. Sollicitudin risus
-            enim felis nunc vitae. Hac molestie feugiat ipsum faucibus tempor
-            vulputate eu. Ac sed interdum cursus proin.
-          </p>
-          <Link to={"/About"}>En savoir plus</Link>
-        </div>
+        <div className=" about__content max-w  padding">
 
-        <img
-          src={require("../img/About-pic.png")}
-          alt="Apropos image représentation"
-        />
+
+          <div className="about__contentInfo">
+
+            <div className="about__contentTxt ">
+            <h2>JCV Consulting</h2>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Faucibus vitae ornare eu
+                mattis pellentesque morbi et duis condimentum. Sollicitudin
+                risus enim felis nunc vitae. Hac molestie feugiat ipsum faucibus
+                tempor vulputate eu. Ac sed interdum cursus proin.
+              </p>
+
+              <Link className="btn about__btn" to={"/About"}>
+                En savoir plus
+              </Link>
+
+            </div>
+
+            <img
+              src={require("../img/hero/home.jpg")}
+              alt="Apropos image représentation"
+              width={328}
+              height={207}
+              loading="lazy"
+            />
+
+          </div>
+
+        </div>
       </section>
 
       <section className="products">
@@ -67,7 +82,8 @@ export default function Home() {
               >
                 <img
                   src={Api.url + category.attributes.pic.data.attributes.url}
-                  alt="" width={300}
+                  alt=""
+                  width={300}
                 />
                 <h4>{category.attributes.name}</h4>
                 <button>Voir Produit</button>
@@ -79,4 +95,3 @@ export default function Home() {
     </div>
   );
 }
-
