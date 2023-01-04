69b5aeb2db8ab0ff4e4ea9eb1ef53f2c4e3ab186
Gauvin jonathan <59934702+Jonathan97480@users.noreply.github.com>
2023-01-04 13:28:44 +0400

    fixe dark thene

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
 import { wrapper } from '../slice/store'
 import { ThemeProvider } from 'next-themes'
-
+import { css, GlobalStyles } from "@mui/material";
 
 
 
@@ -12,7 +12,9 @@ function App({ Component, pageProps }: AppProps) {
 
     <ThemeProvider storageKey='theme' defaultTheme='system' enableSystem={true}>
 
-
+      <GlobalStyles
+        styles={globalStyles}
+      />
       <Layout >
         <Component {...pageProps} />
 
@@ -25,3 +27,28 @@ function App({ Component, pageProps }: AppProps) {
 
 
 export default wrapper.withRedux(App)
+
+
+
+
+export const globalStyles = css`
+  :root {
+    body {
+       --bkg-primary-color: #ffffff;
+      --bkg-secondary-color: #F9F9F9;
+      --txt-primary-color: #454851;
+      --title-secondary-color: #078080;
+      --links-primary-color: #ED593C;
+    }
+  }
+  [data-theme="dark"] {
+    body {
+      --bkg-primary-color: red;
+      --bkg-secondary-color: #F9F9F9;
+      --txt-primary-color: #454851;
+      --title-secondary-color: #078080;
+      --links-primary-color: blue;
+    }
+  }
+`;
+
