6aba11a2dd3d38ae713eb090b98c19417ff262f8
Gauvin jonathan <59934702+Jonathan97480@users.noreply.github.com>
2023-01-11 14:16:32 +0400

    fixe mobile header menu4

--- components/header.tsx
+++ components/header.tsx
@@ -53,7 +53,7 @@ export default function Header() {
 
 
   return (
-    <header className="header padding__header">
+    <header className={isOpen ? "header padding__header header-open" : "header padding__header  header-close"}>
       <div className="header__content max-w">
         <Image
           className="header__burger"
@@ -69,7 +69,7 @@ export default function Header() {
         />
 
         <nav
-          className={isOpen ? "header__nav header__nav-open" : "header__nav"}
+          className={"header__nav header__nav-open"}
         >
           <Link href="/" title="Lien qui redirige vers la page d'accueil">
             <Image
@@ -84,7 +84,7 @@ export default function Header() {
 
           <ul
             className={
-              isOpen ? "header__list header__list-open" : "header__list"
+              "header__list header__list-open"
             }
           >
             <li>
