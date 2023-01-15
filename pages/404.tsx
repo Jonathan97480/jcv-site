90bb07740ccc9a75ac410133f6041af6be560ed5
Maakino <teddyequerre0@gmail.com>
2023-01-15 06:19:37 +0100

    add style one page 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,24 +1,40 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { MyMain } from "../components";
 
 export default function FourOhFour() {
-    return (
-        <div>
-            <Image src="../img/girl-oups.png" alt="404" width={580} height={440} />
-            <div>
-                <h1>Oups, voilà qui n'était pas prévu!</h1>
-                <small>Erreur 404</small>
-                <p>Désolé mais ce lien est introuvable! Dirigez-vous vers la page d'accueil ou contactez nous en cas de questions...</p>
-                <div>
-                    <Link href="/">
-                        <a>Retour à la page d'accueil</a>
-                    </Link>
-                    <Link href="/contact">
-                        <a>contactez nous</a>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <MyMain
+      pageTitle="Page 404"
+      pageDescription="Page d'erreur 404"
+      className="page404"
+    >
+      <div className="max-w padding page404__content">
+        <Image
+          src={require("../img/girl-oups.png")}
+          alt="404"
+          width={580}
+          height={440}
+          loading="lazy"
+        />
+        <div className="page404__info">
+          <h1 className="title title-medium">Oups, voilà qui n'était pas prévu!</h1>
+          <small className="title title-medium">Erreur 404</small>
+          <p>
+            Désolé mais ce lien est introuvable! Dirigez-vous vers la page
+            d'accueil ou contactez nous en cas de questions...
+          </p>
+          <div className="page404__btn">
+            <Link href="/" className="btn">
+              Retour à la page d'accueil
+            </Link>
+            <Link href="/contact" className="btn btn-secondary">
+              contactez nous
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </MyMain>
+  );
 }
